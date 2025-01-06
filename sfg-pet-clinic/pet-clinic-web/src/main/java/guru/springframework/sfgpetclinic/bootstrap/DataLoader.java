ab8f08c89d2251aed0245282c483a1d602cb52ec
Tadeo Guerstein <tguerstein@bykom.com>
2025-01-06 11:01:54 -0300

    refactorizo generación de ID

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Tadeo");
         owner1.setLastName("Guerstein");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Guru");
         owner2.setLastName("SpringFramework");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner{
         System.out.println("--------- Loaded owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Same");
         vet2.setLastName("Axe");
 
