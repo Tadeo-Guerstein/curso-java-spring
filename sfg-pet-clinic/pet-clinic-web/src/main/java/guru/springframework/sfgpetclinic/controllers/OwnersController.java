461b1b891b1c334d9a541c3f733e022f544dd2cf
Tadeo Guerstein <tguerstein@bykom.com>
2025-01-06 10:37:54 -0300

    agrgeo listado de Owners

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -1,13 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String ownerList(){
+    public String ownerList(Model model){
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
