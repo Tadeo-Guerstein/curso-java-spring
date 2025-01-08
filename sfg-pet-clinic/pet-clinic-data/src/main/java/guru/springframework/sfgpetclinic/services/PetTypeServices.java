0e9c1d6c5a68f09a7721a3f682898931ea0d87fe
Tadeo Guerstein <tguerstein@bykom.com>
2025-01-08 10:28:38 -0300

    agrego servicio PetType

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeServices.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeServices extends CrudService<PetType, Long> {
+}
