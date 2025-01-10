923c55950816320eb2b3776d81c919e209b9d59b
Tadeo Guerstein <tguerstein@bykom.com>
2025-01-10 09:28:37 -0300

    agrego repositorios jpa

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/VetRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VetRepository extends CrudRepository<VetRepository, Long> {
+}
